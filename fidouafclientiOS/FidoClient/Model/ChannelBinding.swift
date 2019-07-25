b75abf1ef7b6630331a6e035956cca4440372875
Frederic Jahn <fja@cap3.de>
2019-07-25 14:15:28 +0200

    make an public initializer for UAFMessage & ChannelBinding

--- fidouafclientiOS/FidoClient/Model/ChannelBinding.swift
+++ fidouafclientiOS/FidoClient/Model/ChannelBinding.swift
@@ -6,4 +6,11 @@ public struct ChannelBinding: Codable {
     public let tlsServerCertificate: String?
     public let tlsUnique: String?
     public let cid_pubKey: String?
+    
+    public init(serverEndPoint: String?, tlsServerCertificate: String?, tlsUnique: String?, cid_pubKey: String?) {
+        self.serverEndPoint = serverEndPoint
+        self.tlsServerCertificate = tlsServerCertificate
+        self.tlsUnique = tlsUnique
+        self.cid_pubKey = cid_pubKey
+    }
 }
--- fidouafclientiOS/FidoClient/Model/UAFMessage.swift
+++ fidouafclientiOS/FidoClient/Model/UAFMessage.swift
@@ -2,4 +2,8 @@ import Foundation
 
 public struct UAFMessage: Codable {
     public let uafProtocolMessage: String
+    
+    public init (uafProtocolMessage: String) {
+        self.uafProtocolMessage = uafProtocolMessage
+    }
 }
