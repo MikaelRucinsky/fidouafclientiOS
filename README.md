a53df7c97e3c33a392a15750c858f3e291d566f4
Frederic Jahn <fja@cap3.de>
2019-07-25 15:02:55 +0200

    remove download of trustedFacetList

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 FIDO UAF Client & Authenticator for iOS by Hanko.
 
 **Supported UAF versions:** 1.0
+
+## Installation
+
+### Carthage
+
+To integrate FidoUafClientiOS into your Xcode project using Carthage, specify it in your Cartfile:
+
+```
+github "teamhanko/fidouafclientiOS" == 0.0.5
+```
+
+## Configuration
+
+FidoUafClientiOS uses FaceID, so you have to define `NSFaceIDUsageDescription` in your `Info.plist`.
+
+### OperationPrompts
+
+The FidoUafClientiOS displays 
--- fidouafclientiOS/FidoClient/Services/TrustedFacetsService.swift
+++ fidouafclientiOS/FidoClient/Services/TrustedFacetsService.swift
@@ -2,24 +2,28 @@ import Foundation
 
 class TrustedFacetsService {
     
+    //static private func get(url: String, callback: @escaping ([TrustedFacets]) -> Void) {
+    //    let nsUrl = URL(string: url)
+    //    let session = URLSession.shared
+    //
+    //    let request = URLRequest(url: nsUrl!)
+    //
+    //    let task = session.dataTask(with: request, completionHandler: { data, response, error in
+    //        guard error == nil else {
+    //            callback([])
+    //            return
+    //        }
+    //        guard let responseData = data else {
+    //            callback([])
+    //            return
+    //        }
+    //        callback(TrustedFacets.getArrayFromData(data: responseData))
+    //    })
+    //
+    //    task.resume()
+    //}
+    
     static private func get(url: String, callback: @escaping ([TrustedFacets]) -> Void) {
-        let nsUrl = URL(string: url)
-        let session = URLSession.shared
-        
-        let request = URLRequest(url: nsUrl!)
-        
-        let task = session.dataTask(with: request, completionHandler: { data, response, error in
-            guard error == nil else {
-                callback([])
-                return
-            }
-            guard let responseData = data else {
-                callback([])
-                return
-            }
-            callback(TrustedFacets.getArrayFromData(data: responseData))
-        })
-        
-        task.resume()
+        callback([])
     }
 }
